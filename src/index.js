796ec1e7d4b186d2f8ad0c85fd74a9809c693632
Frank-197 <frankomosh197@gmail.com>
2022-06-15 07:51:51 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
   
-}
+
 
 document.addEventListener('DOMContentLoaded', init);
+const inputForm=document.querySelector('form');
+  inputForm.addEventListener('submit',(event)=>{
+     event.preventDefault()
+     const input=document.querySelector('input#SearchById')
+     fetch()
+     .then(response=>response.json())
+     .then(data=>{
+         const title=document.querySelector('section#MovieDetails h4')
+         const summary=document.querySelector('section#MovieDetails p')
+         title.innerText=data.title
+         summary.innerText=data.summary
+     })
+  })
+}
